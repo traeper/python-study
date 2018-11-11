4a05601c0afc925a3d28c8a5b8573ca583e598ca
jay <traeper@gmail.com>
2018-11-11 21:53:47 +0900

    for loop break, continue

---
+++ class/class.py
@@ -0,0 +1,6 @@
+
+class ForeCast(object):
+
+    def __init__(self):
+        self.priority = 1
+        self.amount = 100.2
---
+++ loop/for_loop_break.py
@@ -0,0 +1,15 @@
+"""
+for 반복문
+
+break
+
+break : 반복문 종료
+"""
+
+arr = [1, 2, 3, 4, 5]
+
+# 숫자 3까지 출력하고 반복문을 종료
+for n in arr:
+    print(n)
+    if n == 3:
+        break
