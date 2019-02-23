e15f2a5758b4435e2d51cfaf6db9b4388062427b
jay <traeper@gmail.com>
2019-02-23 19:26:31 +0900

    duplicate check

---
+++ data_type/list/duplicate_check.py
@@ -0,0 +1,34 @@
+def remove_duplicates(values):
+    output = []
+    seen = set()
+    for value in values:
+        # If value has not been encountered yet,
+        # ... add it to both list and set.
+        if value not in seen:
+            output.append(value)
+            seen.add(value)
+    return output
+
+
+def contains(values, obj):
+    for value in values:
+        # If value has not been encountered yet,
+        # ... add it to both list and set.
+        if obj == value:
+            return True
+
+    return False
+
+# Remove duplicates from this list.
+values = [5, 5, 1, 1, 2, 3, 4, 4, 5]
+result = remove_duplicates(values)
+print(result)
+
+
+values = [('a', 'b', 'd'), ('e','f','g'), 2, 3]
+hasContain = contains(values, 2)
+print(hasContain)
+hasContain2 = contains(values, 10)
+print(hasContain2)
+
+
